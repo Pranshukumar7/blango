79fd438f13fe72cda0ce57c8f8f78c2890fecbaa
Pranshu Kumar <pranshukumar8544@gmail.com>
2023-05-01 19:42:27 +0530

    Implemented Crispy Form

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,6 +1,7 @@
 from django import template
 from django.utils.html import format_html
 from django.contrib.auth import get_user_model
+from blog.models import Post
 user_model = get_user_model()
 register = template.Library()
 @register.filter
@@ -8,7 +9,7 @@ def author_details(author,current_user = None):
     if not isinstance(author,user_model):
         return ""
     if current_user == author:
-        return format_html("<strong>Me<strong>")
+        return format_html("<strong>Me</strong>")
     if author.first_name and author.last_name:
         name = (f"{author.first_name} {author.last_name}")
     else:
@@ -21,3 +22,15 @@ def author_details(author,current_user = None):
         suffix = ""
     return format_html('{}{}{}',prefix,name,suffix)
 
+@register.simple_tag
+def row(extra_classes = ""):
+    return format_html('<div class = "row {}">',extra_classes)
+
+@register.simple_tag
+def endrow():
+    return format_html("</div>")
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(posts):
+    posts = Post.objects.exclude(pk=posts.pk)[:5]
+    return {"title":"Recent Posts","posts":posts}
